29108c85222eff821d239cf28d334365fb94418e
Dor Ben Dor <agesonera@gmail.com>
2023-01-17 12:48:21 +0200

    added basic test suit for project

---
+++ TestSuit/RNG_TestSuit/TestSuitA.cs
@@ -0,0 +1,16 @@
+namespace RNG_TestSuit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
